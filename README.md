1ba8a2708fa09c6b2e8e1bab06c920a5b3d86a5c
Yann <yann.richet@laposte.net>
2017-11-11 14:30:45 +0100

    fix issue https://github.com/yannrichet/rsession/issues/5

--- README.md
+++ README.md
@@ -12,38 +12,34 @@ import static org.math.R.*;
 ...
  
     public static void main(String args[]) {
-        Rsession s = RserveSession.newInstanceTry(System.out);
- 
-        double[] rand = (double[]) s.eval("rnorm(10)",null); //create java variable from R command
- 
-        ...
- 
+        Rsession s = RserveSession.newInstanceTry(System.out, null);
+
+        double[] rand = (double[]) s.eval("rnorm(10)"); //create java variable from R command
+
+        //...
         s.set("c", Math.random()); //create R variable from java one
- 
+
         s.save(new File("save.Rdata"), "c"); //save variables in .Rdata
         s.rm("c"); //delete variable in R environment
         s.load(new File("save.Rdata")); //load R variable from .Rdata
- 
-        ...
- 
+
+        //...
         s.set("df", new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}, "x1", "x2", "x3"); //create data frame from given vectors
-        double value= (Double) (cast(s.eval("df$x1[3]"))); //access one value in data frame
- 
-        ...
- 
+        double value = (double) (s.eval("df$x1[3]")); //access one value in data frame
+
+        //...
         s.toJPEG(new File("plot.jpg"), 400, 400, "plot(rnorm(10))"); //create jpeg file from R graphical command (like plot)
- 
+
         String html = s.asHTML("summary(rnorm(100))"); //format in html using R2HTML
         System.out.println(html);
- 
+
         String txt = s.asString("summary(rnorm(100))"); //format in text
         System.out.println(txt);
- 
-        ...
- 
+
+        //...
         System.out.println(s.installPackage("sensitivity", true)); //install and load R package
         System.out.println(s.installPackage("wavelets", true));
- 
+
         s.end();
     }
 ```
--- src/test/java/org/math/R/Issues.java
+++ src/test/java/org/math/R/Issues.java
@@ -34,11 +34,7 @@ public class Issues {
 
             public void close() {
             }
-        };/*RLogPanel();
-         JFrame f = new JFrame("RLogPanel");
-         f.setContentPane((RLogPanel) l);
-         f.setSize(600, 600);
-         f.setVisible(true);*/
+        };
 
         String http_proxy_env = System.getenv("http_proxy");
         Properties prop = new Properties();
@@ -92,10 +88,42 @@ public class Issues {
         y = s.asString(s.rawEval(string));
 
         System.out.println(y);
-        
+
         assert y.contains("8") : "Bad multi-line eval";
 
-        s.close();
+        s.end();
     }
 
+    @Test
+    public void code_snippet_does_not_compile() throws Exception {
+        Rsession s = RserveSession.newInstanceTry(System.out, null);
+
+        double[] rand = (double[]) s.eval("rnorm(10)"); //create java variable from R command
+
+        //...
+        s.set("c", Math.random()); //create R variable from java one
+
+        s.save(new File("save.Rdata"), "c"); //save variables in .Rdata
+        s.rm("c"); //delete variable in R environment
+        s.load(new File("save.Rdata")); //load R variable from .Rdata
+
+        //...
+        s.set("df", new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}, "x1", "x2", "x3"); //create data frame from given vectors
+        double value = (double) (s.eval("df$x1[3]")); //access one value in data frame
+
+        //...
+        s.toJPEG(new File("plot.jpg"), 400, 400, "plot(rnorm(10))"); //create jpeg file from R graphical command (like plot)
+
+        String html = s.asHTML("summary(rnorm(100))"); //format in html using R2HTML
+        System.out.println(html);
+
+        String txt = s.asString("summary(rnorm(100))"); //format in text
+        System.out.println(txt);
+
+        //...
+        System.out.println(s.installPackage("sensitivity", true)); //install and load R package
+        System.out.println(s.installPackage("wavelets", true));
+
+        s.end();
+    }
 }
