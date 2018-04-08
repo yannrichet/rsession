bdc6814dddcca8db698a99de1d76b1c854f7f40b
Yann Richet <richetyann@gmail.com>
2018-04-08 19:24:00 +0200

    raw test for library() use in renjin

--- src/test/java/org/math/R/RenjinLibraryTest.java
+++ src/test/java/org/math/R/RenjinLibraryTest.java
@@ -1,5 +1,6 @@
 package org.math.R;
 
+import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import org.renjin.aether.AetherPackageLoader;
@@ -8,6 +9,8 @@ import org.renjin.eval.SessionBuilder;
 import org.renjin.primitives.packaging.PackageLoader;
 import org.renjin.script.RenjinScriptEngine;
 import org.renjin.script.RenjinScriptEngineFactory;
+import org.renjin.sexp.LogicalArrayVector;
+import org.renjin.sexp.StringArrayVector;
 
 /**
  *
@@ -22,13 +25,23 @@ public class RenjinLibraryTest {
     }
 
     @Test
+    public void testLoadPackage() throws Exception {
+        Object ret = R.eval("library('pso',logical.return=T,quietly=T,verbose=F)");
+        System.err.println(">> " + ret + " (" + ret.getClass() + ")");
+        assert ((LogicalArrayVector) ret).isElementTrue(0) : "Cannot load pso";
+    }
+
+    @Test
     public void testInstallPackage() throws Exception {
-        R.eval("library('pso')");
+        Object ret = R.eval("library('pso')");
+        System.err.println(">> " + ret + " (" + ret.getClass() + ")");
+        String[] a = ((StringArrayVector) ret).toArray();
+        assert Arrays.asList(a).contains("pso") : "Cannot find pso in " + Arrays.asList(a);
     }
 
     @Before
     public void setUp() throws Exception {
-        Session session = new SessionBuilder().bind(PackageLoader.class, new AetherPackageLoader()).build();
+        Session session = new SessionBuilder().bind(PackageLoader.class, new AetherPackageLoader()).withDefaultPackages().build();
         R = new RenjinScriptEngineFactory().getScriptEngine(session);
         if (R == null) {
             throw new RuntimeException("Renjin Script Engine not found in the classpath.");
