e340bee0391a39c6cf19cfc4e2a28915d3176ca5
Yann Richet <richetyann@gmail.com>
2019-07-09 11:20:24 +0200

    Update RserveSessionTest.java

--- src/test/java/org/math/R/RserveSessionTest.java
+++ src/test/java/org/math/R/RserveSessionTest.java
@@ -87,7 +87,7 @@ public class RserveSessionTest {
 
     @Test
     public void testInstallPackages() {
-        String out = s.installPackage("sensitivity", true);
+        String out = s.installPackage("boot", true);
         assert out.equals(Rsession.PACKAGELOADED) : "Failed to load package sensitivity: " + out;
         String out2 = s.installPackage("pso", true);
         assert out2.equals(Rsession.PACKAGELOADED) : "Failed to load package pso: " + out2;
