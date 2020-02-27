8b958468e4c284f0819358b2f02e8f35af824667
Yann Richet <richetyann@gmail.com>
2020-02-27 13:07:59 +0100

    Update BasicTest.java

--- src/test/java/org/math/R/BasicTest.java
+++ src/test/java/org/math/R/BasicTest.java
@@ -290,7 +290,7 @@ public class BasicTest {
         assert r.asDouble(r.eval("lm$m1[2]")) == 2.0 : "Bad values in list: " + r.eval("print(lm)");
 
         assert r.set("la", r.asMatrix(r.eval("a")), "a1") : "Failed to create list";
-        assert r.print("la").contains("a1") && r.print("la$a1").contains("[1] 0 1") : "Bad print: " + r.print("la")+ r.print("la$a1");
+        assert r.print("la").contains("a1") && r.print("la$a1").contains("[1] 0 1") : "Bad print: la=" + r.print("la")+ "\nla$a1="+r.print("la$a1");
 
         assert r.set("ld", r.asMatrix(r.eval("d")), "d1") : "Failed to create list";
         assert r.print("ld").contains("d1") && r.print("ld$d1").contains("[1] 0") : "Bad print: " + r.print("ld");
@@ -324,7 +324,7 @@ public class BasicTest {
         assert s.asDouble(s.eval("lm$m1[2]")) == 2.0 : "Bad values in list: " + s.eval("print(lm)");
 
         assert s.set("la", s.asMatrix(s.eval("a")), "a1") : "Failed to create list";
-        assert s.print("la").contains("a1") && r.print("la$a1").contains("[1] 0 1") : "Bad print: " + s.print("la")+ r.print("la$a1");
+        assert s.print("la").contains("a1") && r.print("la$a1").contains("[1] 0 1") : "Bad print: la=" + s.print("la")+ "\nla$a1="+ r.print("la$a1");
 
         assert s.set("ld", s.asMatrix(s.eval("d")), "d1") : "Failed to create list";
         assert s.print("ld").contains("d1") && s.print("ld$d1").contains("[1] 0") : "Bad print: " + s.print("ld");
