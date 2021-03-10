c2576d3d1489a42c3ecf22ac90ce0c71779aed73
richet <richetyann@gmail.com>
2021-03-10 22:08:40 +0100

    turn around java cannot set its own env variables

--- src/main/java/org/math/R/R2jsSession.java
+++ src/main/java/org/math/R/R2jsSession.java
@@ -1491,32 +1491,20 @@ public class R2jsSession extends Rsession implements RLog {
         return result;
     }
 
-    @SuppressWarnings("unchecked")
-    public static void setEnv(String k, String v) throws Exception {
-        try {
-            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-            theEnvironmentField.setAccessible(true);
-            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-            env.put(k, v);
-            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-            theCaseInsensitiveEnvironmentField.setAccessible(true);
-            Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
-            cienv.put(k, v);
-        } catch (NoSuchFieldException e) {
-            Class[] classes = Collections.class.getDeclaredClasses();
-            Map<String, String> env = System.getenv();
-            for (Class cl : classes) {
-                if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-                    Field field = cl.getDeclaredField("m");
-                    field.setAccessible(true);
-                    Object obj = field.get(env);
-                    Map<String, String> map = (Map<String, String>) obj;
-                    map.clear();
-                    map.put(k, v);
-                }
-            }
-        }
+    static Map<String, String> Env = new HashMap();
+
+    public static void setEnv(String k, String v) {
+        Env.put(k, v);
+    }
+
+    public static String getEnv(String k) {
+        if (Env.containsKey(k))
+            return Env.get(k);
+        if (System.getenv().containsKey(k))
+            return System.getenv().get(k);
+        if (System.getProperties().containsKey(k))
+            return System.getProperties().get(k).toString();
+        return "";
     }
 
     /**
