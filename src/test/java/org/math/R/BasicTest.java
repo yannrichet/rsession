4a6a1c13590793bbe3ebc4f03fc640c393e01e6e
yannrichet <richetyann@gmail.com>
2021-11-22 16:58:00 +0100

    fix test ignoring http_proxy

--- src/test/java/org/math/R/BasicTest.java
+++ src/test/java/org/math/R/BasicTest.java
@@ -52,7 +52,7 @@ public class BasicTest {
         
         if (!(tmpdir.isDirectory() || tmpdir.mkdirs())) throw new IllegalArgumentException("Failed to create temp dir");
 
-        s = new RserveSession(l, null,null);
+        s = new RserveSession(l, prop,null);
         System.out.println("| R.version:\t" + s.eval("R.version.string"));
         System.out.println("| Rserve.version:\t" + s.eval("installed.packages(lib.loc='" + RserveDaemon.app_dir() + "')[\"Rserve\",\"Version\"]"));
 
