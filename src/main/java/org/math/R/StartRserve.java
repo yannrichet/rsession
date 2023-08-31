51a0fdb3f98f34b2c7ff0d24fd91160096b589ac
Yann Richet <yann.richet@laposte.net>
2023-08-31 16:24:15 +0200

    support & test R 4.4 behavior

--- src/main/java/org/math/R/StartRserve.java
+++ src/main/java/org/math/R/StartRserve.java
@@ -310,6 +310,8 @@ public class StartRserve {
             } else if (outv_str.startsWith("4.3")) {
                 R_version_path = "R-4.3";
                 Rserve_version = "1.8-11";
+            else if (outv_str.startsWith("4.")) {
+                Rserve_version = "1.8-11";
             } else Log.Err.println("R version ('"+outv_str+"') not supported.\n  Will try to use source install."+ (isWindows()?" (assuming Rtools is available)":""));
         } catch (Exception ex) {
             Log.Err.println(ex.getMessage()+": \n"+outv_str);
