82737ced0ee78136b3129ff9ec7d5e0186de440b
Nicolas CHABALIER <nicolas.chabalier-artenum@irsn.fr>
2025-03-22 11:54:23 +0100

    Fix merge problem.

--- src/main/java/org/math/R/executors/NashornExecutor.java
+++ src/main/java/org/math/R/executors/NashornExecutor.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/org/math/R/executors/NashornExecutor.java
 package org.math.R.executors;
-========
-package org.math.R;
->>>>>>>> master:src/main/java/org/math/R/R2jsSession.java
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
